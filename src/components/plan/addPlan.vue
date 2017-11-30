abb02dd2d2ec7905d1365a89352fca19d3b5c215
xiaolan <1275030640@qq.com>
2017-12-01 07:24:27 +0800

    export

--- src/components/plan/addPlan.vue
+++ src/components/plan/addPlan.vue
@@ -8,19 +8,14 @@
 
 <script>
 
-  import conf from '@/constant/export'
+  import {param1,param2} from '@/constant/export'
+  import p1 , * as coo  from '@/constant/export'
 
 
-  import * as param from '@/constant/export-default'
-  import paramDefault from '@/constant/export-default'
+  console.log(param1,param2)
+  console.log(p1)
+  console.log(coo)
 
-
-  import {param1,param2} from '@/constant/export-default'
-
-  console.log("conf---->",conf)
-  console.log("param---->",param)
-  console.log("paramDefault---->",paramDefault)
-  console.log("param1,param2---->",param1,param2)
 
   export default {
     name:"增加计划",
--- src/constant/export.js
+++ src/constant/export.js
@@ -6,3 +6,6 @@ export const param1 = "param1";
 export const param2 = "param2";
 export const param3 = "param3";
 export const param4 = "param4";
+export {param4 as param5};
+
+export default param1;
